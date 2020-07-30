500ce5bf897f793f2651ab6101fdf09b5bbf5175
khan454 <50718502+khan454@users.noreply.github.com>
2020-07-30 12:07:25 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,12 @@
+import { Books } from '../../app/bookInfo'
+
+export const MyBooks: Books[] = [
+    { bookName: 'A Wizard of Earthsea', author: 'Ursula K. Le Guin', genre: 'Fantasy',
+        yearPub: 1968, picture: 'assets/images/woe_cover.jpg' },
+    { bookName: 'The Expanse: Leviathan Wakes', author: 'James S. A. Correy', genre: 'Science Fiction',
+        yearPub: 2011, picture: 'assets/images/lew_cover.jpg' },
+    { bookName: 'The Lord of the Rings: The Fellowship of the Ring', author: 'J.R.R. Tolkien',
+        genre: 'Fantasy', yearPub: 1954, picture: 'assets/images/fotr_cover.jpg'},
+    { bookName:'A Song of Ice and Fire: A Game of Thrones', author: 'George R. R. Martin',
+        genre: 'Fantasy', yearPub: 1996, picture: 'assets/images/got_cover.jpg'}
+];
